380ff6c5b8d5268681d8ba5122f658a04d9b1296
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-16 17:09:24 -0500

    First commit

---
+++ src/app/auth/pages/layaoutPage/layaoutPage.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layaoutPage',
+  templateUrl: './layaoutPage.component.html',
+})
+export class LayaoutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
