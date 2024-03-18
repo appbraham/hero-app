8158f5771681ac7b1b8429a276c5cfa091e4032d
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-17 20:16:37 -0500

    add lazy loading to auth and heroes module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayaoutPageComponent } from './pages/layaoutPage/layaoutPage.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayaoutPageComponent,
+    children:[
+      {
+        path: 'login',
+        component: LoginPageComponent
+      },
+      {
+        path: 'new-account',
+        component: RegisterPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [ RouterModule.forChild(routes) ],
