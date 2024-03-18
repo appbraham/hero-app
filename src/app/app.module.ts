c1df0c2ba5f1f583a903d7f7114b5e71e88471c7
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-18 13:52:08 -0500

    add sidenav, toolbar and json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
     AppRoutingModule,
     SharedModule,
   ],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 import { LayaoutPageComponent } from './pages/layaoutPage/layaoutPage.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -14,8 +15,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     RegisterPageComponent,
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    MaterialModule
   ]
 })
 export class AuthModule { }
